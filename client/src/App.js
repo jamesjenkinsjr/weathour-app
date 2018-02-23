5a944e62c8ae3bad28c95e9719f4a9a8984026ff
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-23 18:55:54 -0500

    Additional files shells added, create basic app layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      lat: 1,
+      long: -1,
+      zip: '',
+      hourlyWeather: {},
+      error: null
+    }
+    this.handleZip = this.handleZip.bind(this);
+  }
+  handleZip(e) {
+    this.setState({
+      zip: +e.target.value
+    });
+  }
+  
   render() {
     return (
       <div>
-        <p>Swag is ballin'</p>
+        <div className="header">
+          <h1>Hourly Weather</h1>
+          <p>Enter your zip code below to retrieve an hourly outlook on weather in your area!</p>
+        </div>
+        <form>
+          <label>Zip Code <input type="number" placeholder="ex. 90210" value={this.state.zip} onChange={(e) => {this.handleZip(e)}}/></label>
+          <button type="submit">Generate</button>
+        </form>
       </div>
     );
   }
