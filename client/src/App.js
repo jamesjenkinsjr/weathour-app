fbaaf1f33ffa96f6bc9253b706382f3a200dfe34
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-23 19:22:21 -0500

    Allow input update + activate submit for coordinates form

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,39 @@ class App extends Component {
     super();
     this.state = {
       lat: 1,
-      long: -1,
+      long: 1,
       zip: '',
       hourlyWeather: {},
       error: null
     }
     this.handleZip = this.handleZip.bind(this);
+    this.handleSubmitCoordinates = this.handleSubmitCoordinates.bind(this);
+    this.handleSubmitZip = this.handleSubmitZip.bind(this);
+    this.handleLat = this.handleLat.bind(this);
+    this.handleLong = this.handleLong.bind(this);
   }
   handleZip(e) {
     this.setState({
       zip: +e.target.value
     });
+  }
+  handleLat(e) {
+    this.setState({
+      lat: +e.target.value
+    });
+  }
+  handleLong(e) {
+    this.setState({
+      long: +e.target.value
+    });
+  }
+
+  handleSubmitCoordinates(e) {
+    alert('eventually latitude and longitude');
+  }
+
+  handleSubmitZip(e) {
+    alert('eventually zip results');
   }
   
   render() {
@@ -24,9 +46,14 @@ class App extends Component {
       <div>
         <div className="header">
           <h1>Hourly Weather</h1>
-          <p>Enter your zip code below to retrieve an hourly outlook on weather in your area!</p>
+          <p>Enter your lattitude and longitude, or zip code, below to retrieve an hourly outlook on weather in your area!</p>
         </div>
-        <form>
+        <form onSubmit={(e) => {this.handleSubmitCoordinates(e)}}>
+          <label>Latitiude <input type="number" value={this.state.lat} onChange={(e) => this.handleLat(e)}/></label>
+          <label>Longitude <input type="number" value={this.state.long} onChange={(e) => this.handleLong(e)}/></label>
+          <button type="submit">Generate</button>
+        </form>
+        <form onSubmit={(e) => {this.handleSubmitZip(e)}}>
           <label>Zip Code <input type="number" placeholder="ex. 90210" value={this.state.zip} onChange={(e) => {this.handleZip(e)}}/></label>
           <button type="submit">Generate</button>
         </form>
